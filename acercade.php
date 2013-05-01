d9c2c62463163425c9b6512fe008e4bdee9fd851
Luis Alvarado <luis.alvarado@boszDigital>
2013-04-30 21:36:45 -0600

    Adding new page.

---
+++ acercade.php
@@ -0,0 +1,39 @@
+<?php
+//Header
+	$cssPage ='firstRound';
+	$title = 'Acerca de';
+	$classBody="firstRound";
+	$ifNav="yes";
+	$backPage="index.php";
+	include('includes/header.php');
+	
+?>
+
+<div class="row">
+	<div class="large-12 columns">
+    	<h2>Olimpiadas Especiales Costa Rica</h2>
+	</div>
+</div>
+
+<div class="row">
+  <div class="large-5 columns">
+  	<img src="images/winners.jpg">
+    <br>
+  </div>
+  <div class="large-7 columns">
+    <h5 class="subheader">Olimpiadas Especiales es una organizaci&oacute;n sin fines de lucro dedicada a mejorar la calidad de vida de las personas con discapacidad intelectual por medio del entretenimiento y la competici&oacute;n deportiva durante todo el año. <br/> 
+    En nuestro pa&iacute;s, la Asociaci&oacute;n Olimpiadas Especiales tiene 23 años de vigencia, empez&oacute; formalmente en 1987; con el avance de los años ha sido fundamental ya que el deporte se convirti&oacute; en la ventana mediante la cual cientos de atletas encontraron por m&eacute;ritos propios su espacio en la vida cotidiana. <br/>  
+   Actualmente el Programa Olimpiadas Especiales Costa Rica est&aacute; subdividida en 13 regiones que cubren todo el territorio nacional.<br/>
+   Las oficinas centrales se ubican en San Jos&eacute;, San Francisco de Dos R&iacute;os, detr&aacute;s de la emisora Faro del Caribe. Tel&eacute;fono: 2286-6060.<br/>
+   Costa Rica es una de las principales organizaciones de Latinoam&eacute;rica regi&oacute;n que adem&aacute;s tiene programas nacionales en Argentina, Bolivia, Brasil, Chile, Cuba, Ecuador, El Salvador, Guatemala, Honduras, Panam&aacute;, Paraguay, Per&uacute;, Puerto Rico, Rep&uacute;blica Dominicana, Uruguay y Venezuela.</h5>
+  </div>
+</div>
+
+<div class="row">
+	<div class="large-12 columns">
+    	Visite nuestro sitio web oficial: <a href="http://www.olimpiadasespeciales.cr">http://www.olimpiadasespeciales.cr/</a>
+    </div>
+</div>
+<?php
+	include('includes/footer.php');
+?>
