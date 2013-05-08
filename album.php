8c900878c1405b76ca4a019aecc1ed18d9a3c184
Carolina Trejos <carolina.trejos@boszdigital.com>
2013-05-07 20:07:50 -0600

    adding token

--- album.php
+++ album.php
@@ -7,25 +7,28 @@
 	$backPage="index.php";
 	include('includes/header.php');
 	
-	$FBid = 'phraseit.cr';
-	$FBAlbumId='147672652062673';
+	require 'src/facebook.php';
 	
-	//Get the contents of a Facebook page
-	$FBpage = file_get_contents('https://graph.facebook.com/'.$FBid.'/albums');
-	
-	//Interpret data with JSON
-	$photoData = json_decode($FBpage);
+	// Create our Application instance (replace this with your appId and secret).
+	$facebook = new Facebook(array(
+			'appId'  => '167574096737934',
+			'secret' => 'e1d29c634bb1a9701c69d31e7ff097aa',
+	));
+	$FBid = '10151379786557121';
+	$access_token = $facebook->getAccessToken();
+	$graph_url= "https://graph.facebook.com/". $FBid ."/photos?"
+			. "access_token=" .$access_token;
 	
 ?>
 <div class="row">
 	<ul class="small-block-grid-3 large-block-grid-4">
 <?php
 
-	$rawAlbumData = file_get_contents('https://graph.facebook.com/147672652062673/photos');
+	$rawAlbumData = file_get_contents($graph_url);
 	//Interpret data with JSON
 	$photoData = json_decode($rawAlbumData);
 	$index=0;
-	foreach($photoData->data as $data){
+	foreach($photoData->data[0]->images as $data){
 		
 		echo '<li><a href="#" data-reveal-id="myModal'.$index.'"><img src="'.$data->source.'" /></a></li>';
 		echo '<li id="myModal'.$index.'" class="small reveal-modal"><img src="'.$data->source.'" /><a class="close-reveal-modal">&#215;</a></li>';
@@ -34,6 +37,7 @@
 ?>
 	</ul>
 </div>
+
 <?php
 	include('includes/footer.php');
 ?>
